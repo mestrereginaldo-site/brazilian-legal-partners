06e2f1589df5b9b79fdfd29c6c6cf746f2d45d3a
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-15 12:57:30 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
 import './globals.css'
-import { Inter, Playfair_Display } from 'next/font/google'
 import AdSenseScript from './components/AdSenseScript'
-
-const inter = Inter({ 
-  subsets: ['latin'],
-  variable: '--font-inter'
-})
-
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-playfair'
-})
 
 export const metadata = {
   title: 'Brazilian Legal Partners | Premier Business Law Experts',
@@ -23,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
+    <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
